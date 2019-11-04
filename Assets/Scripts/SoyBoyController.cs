8b3ed48800c3fb2421ca8e6de27cf9fc06c53601
Chad Bissonnette [Student] <chad.bissonnette@student.sl.on.ca>
2019-11-04 15:42:10 -0500

    Hooking up character animations

--- Assets/Scripts/SoyBoyController.cs
+++ Assets/Scripts/SoyBoyController.cs
@@ -116,6 +116,9 @@ public class SoyBoyController : MonoBehaviour
         input.x = Input.GetAxis("Horizontal");
         input.y = Input.GetAxis("Jump");
 
+        animator.SetFloat("Speed", Mathf.Abs(input.x));
+
+
         if (input.x > 0f)
         {
             sr.flipX = false;
@@ -128,10 +131,12 @@ public class SoyBoyController : MonoBehaviour
         if (input.y >= 1f)
         {
             jumpDuration += Time.deltaTime;
+            animator.SetBool("IsJumping", true);
         }
         else
         {
             isJumping = false;
+            animator.SetBool("IsJumping", false);
             jumpDuration = 0f;
         }
 
@@ -141,6 +146,7 @@ public class SoyBoyController : MonoBehaviour
             {
                 isJumping = true;
             }
+            animator.SetBool("IsOnWall", false);
         }
 
         if (jumpDuration > jumpDurationThreshold) input.y = 0f;
@@ -184,11 +190,24 @@ public class SoyBoyController : MonoBehaviour
 
         if (IsWallToLeftOrRight() && !PlayerIsOnGround() && input.y == 1)
         {
-            rb.velocity = new Vector2(-GetWallDirection()
-            * speed * 0.75f, rb.velocity.y);
+            rb.velocity = new Vector2(-GetWallDirection() * speed 
+            * 0.75f, rb.velocity.y);
+            animator.SetBool("IsOnWall", false);
+            animator.SetBool("IsJumping", true);
         }
 
-        if (isJumping && jumpDuration < jumpDurationThreshold)
+        else if (!IsWallToLeftOrRight())
+        {
+            animator.SetBool("IsOnWall", false);
+            animator.SetBool("IsJumping", true);
+        }
+
+        if (IsWallToLeftOrRight() && !PlayerIsOnGround())
+        {
+            animator.SetBool("IsOnWall", true);
+        }
+
+       if (isJumping && jumpDuration < jumpDurationThreshold)
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpSpeed);
         }
